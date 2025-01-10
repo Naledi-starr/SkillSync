2c1932539f2ebdf632d8daf7239039f6304f1483
namotswjhb024 <namotswjhb024@student.wethinkcode.co.za>
2025-01-10 16:15:39 +0200

    Auth

--- firebase_auth.py
+++ firebase_auth.py
@@ -1,1 +1,42 @@
+import pyrebase
 
+firebaseConfig = {
+
+  "apiKey": "AIzaSyA_1u-yUULkNFOqngiStL3YLmQobHkDXrw",
+
+  "authDomain": "skillsync-14532.firebaseapp.com",
+
+  "projectId": "skillsync-14532",
+
+  "storageBucket": "skillsync-14532.firebasestorage.app",
+
+  "messagingSenderId": "394660088399",
+
+  "appId": "1:394660088399:web:6e90ba87f6381217c2f83a",
+
+  "measurementId": "G-115SYLPZ5V"
+
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+
+def signup():
+    email = input("Enter email: ")
+    password = input("Enter password: ")
+    try:
+        user = auth.create_user_with_email_and_password(email, password)
+        print("Successfully signed up")
+    except:
+        print("Email already exists")
+
+def login():
+
+
+
+answer = input("Are you a new user? [y/n]")   
+if answer == 'yes':
+    signup()
+elif answer == 'n':
+    login()
+#comment
